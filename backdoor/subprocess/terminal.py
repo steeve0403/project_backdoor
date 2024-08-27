c858d7cf2e0f34dd78043d0feca07a279e68defd
steeve0403 <zychsteeve4@gmail.com>
2024-08-27 15:52:09 +0200

    ask command to user

--- backdoor/subprocess/terminal.py
+++ backdoor/subprocess/terminal.py
@@ -4,8 +4,12 @@ import subprocess
 # Popen -> Old Interface
 # run : execute the command and wait for the result
 
-results = subprocess.run("ls -l ", shell=True, capture_output=True, universal_newlines=True) # macos command
+while True:
+    command = input("Command : ")
+    if command == "exit":
+        break
+    results = subprocess.run("ls -l ", shell=True, capture_output=True, universal_newlines=True) # macos command
 
-# print(results.stdout.decode("utf-8", errors="ignore"))
-print(results.stdout)
-print(results.stderr)
+    # print(results.stdout.decode("utf-8", errors="ignore"))
+    print(results.stdout)
+    print(results.stderr)
