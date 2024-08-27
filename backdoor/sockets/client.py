2ff8048fc5cb5ab1adb36262b2061bbf84d27d68
steeve0403 <zychsteeve4@gmail.com>
2024-08-27 12:20:07 +0200

    Manage errors

--- backdoor/sockets/client.py
+++ backdoor/sockets/client.py
@@ -6,8 +6,12 @@ HOST_PORT = 32000
 
 s = socket.socket()
 
-print(f"Connection at server: {HOST_IP}, port: {HOST_PORT} ....")
-s.connect((HOST_IP, HOST_PORT))
-print(f"Connected to server.")
+try:
+    print(f"Connection at server: {HOST_IP}, port: {HOST_PORT} ....")
+    s.connect((HOST_IP, HOST_PORT))
+except ConnectionRefusedError:
+    print(f"Error : Unable to connect to server.")
+else:
+    print(f"Connected to server.")
 
 s.close()
