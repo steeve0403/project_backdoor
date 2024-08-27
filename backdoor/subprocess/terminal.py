3688e55893ede6c65033ec18f304c416bdb1c18f
steeve0403 <zychsteeve4@gmail.com>
2024-08-27 15:42:34 +0200

    retrieve output

--- backdoor/subprocess/terminal.py
+++ backdoor/subprocess/terminal.py
@@ -4,4 +4,8 @@ import subprocess
 # Popen -> Old Interface
 # run : execute the command and wait for the result
 
-subprocess.run("ls -l ", shell=True) # macos command
+results = subprocess.run("ls -l ", shell=True, capture_output=True, universal_newlines=True) # macos command
+
+# print(results.stdout.decode("utf-8", errors="ignore"))
+print(results.stdout)
+print(results.stderr)
