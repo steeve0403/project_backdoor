0c611abc4fd432516217cec4442aeae041cbd8b2
steeve0403 <zychsteeve4@gmail.com>
2024-08-27 12:30:20 +0200

    Share data

--- backdoor/sockets/client.py
+++ backdoor/sockets/client.py
@@ -4,6 +4,7 @@ import time
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
+MAX_DATA_SIZE = 1024
 
 print(f"Connection at server: {HOST_IP}, port: {HOST_PORT} ....")
 while True:
@@ -18,5 +19,9 @@ while True:
         print(f"Connected to server.")
         break
 
-
+received_data = s.recv(MAX_DATA_SIZE)
+if received_data:
+    print(received_data.decode())
+else:
+    print("No data")
 s.close()
--- backdoor/sockets/server.py
+++ backdoor/sockets/server.py
@@ -5,6 +5,8 @@ HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 
 s = socket.socket()
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#  To solve the problem "already used" on Windows, you need to add an option to the socket.
 s.bind((HOST_IP, HOST_PORT))
 s.listen()
 
@@ -12,4 +14,10 @@ print(f"Waiting for connection on {HOST_IP}, port {HOST_PORT} ....")
 connection_socket, client_address = s.accept()
 print(f"Accepted connection from {client_address}")
 
+text_to_send = "Hello"
+
+connection_socket.sendall(text_to_send.encode())
+
 s.close()
+connection_socket.close()
+
